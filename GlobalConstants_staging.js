b790afaa4914663645e06d8f296783aef6b21c4f
Katie Rose <rose_katherine@bah.com>
2016-03-28 19:25:50 -0400

    Update staging url

--- GlobalConstants_staging.js
+++ GlobalConstants_staging.js
@@ -1,6 +1,6 @@
 // Production API path
 export const kGlobalConstants = {
-    API: 'http://ec2-52-6-2-123.compute-1.amazonaws.com:80/v1/',
+    API: 'http://ec2-52-23-175-190.compute-1.amazonaws.com:80/v1/',
     LOCAL_ROOT : '',
     GITHUB: 'https://github.com/fedspendingtransparency/data-act-validator/tree/development',
     GA_TRACKING_ID: 'UA-75081254-1',
